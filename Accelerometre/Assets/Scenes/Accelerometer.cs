4c013401bd96d5ef7dab70be78ff729a1efdbc43
Tsiky Ravonison <tsikkaren@gmail.com>
2022-07-19 17:55:02 +0200

    S2: Creation Application Android. Nommage fichiers csv et reset des tableaux

--- Accelerometre/Assets/Scenes/Accelerometer.cs
+++ Accelerometre/Assets/Scenes/Accelerometer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.UI;
 
 public class Accelerometer : MonoBehaviour
 {
@@ -10,7 +11,14 @@ public class Accelerometer : MonoBehaviour
     [SerializeField] public List<float> dataAccZ = new List<float>();
 
     [SerializeField] public bool clickStart;
+    [SerializeField] public string NameX;
+    [SerializeField] public string NameY;
+    [SerializeField] public string NameZ;
 
+    public GameObject inputFieldX;
+    public GameObject inputFieldY;
+    public GameObject inputFieldZ;
+    
     private string separator = ";";
 
      public void StartAcc()
@@ -21,19 +29,26 @@ public class Accelerometer : MonoBehaviour
     public void StopAcc()
     {
         clickStart = false;
+        NameX = inputFieldX.GetComponent<Text>().text;
+        NameY = inputFieldY.GetComponent<Text>().text;
+        NameZ = inputFieldZ.GetComponent<Text>().text;
 
         string saveStringX = string.Join(separator, dataAccX);
-        File.WriteAllText(Application.dataPath + "/dataX.csv", saveStringX);
+        File.WriteAllText(Application.dataPath + "/" + NameX, saveStringX);
         Debug.Log("Sauvegarde de X");
 
         string saveStringY = string.Join(separator, dataAccY);
-        File.WriteAllText(Application.dataPath + "/dataY.csv", saveStringY);
+        File.WriteAllText(Application.dataPath + "/" + NameY, saveStringY);
         Debug.Log("Sauvegarde de Y");
 
         string saveStringZ = string.Join(separator, dataAccZ);
-        File.WriteAllText(Application.dataPath + "/dataZ.csv", saveStringZ);
+        File.WriteAllText(Application.dataPath + "/" + NameZ, saveStringZ);
         Debug.Log("Sauvegarde de Z");
 
+        dataAccX.Clear();
+        dataAccY.Clear();
+        dataAccZ.Clear();
+        Debug.Log("Reset des tableaux");
     }
 
 
