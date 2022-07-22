97e7575dc9713d2c3effe2c3720c8821b59d8695
Tsiky Ravonison <tsikkaren@gmail.com>
2022-07-22 15:18:11 +0200

    S2: Création du script Matlab. Script + Fichier csv test

---
+++ Matlab_Script/Accelerometre.m
@@ -0,0 +1,22 @@
+M1=readmatrix('test.csv')      %Importation du fichier csv dans une matrice
+
+Fe=200;                         %Frequence d'echantillonage, frequence de l'accelerometre
+Te=1/Fe;
+N=length(M1);
+
+t = [0:(N-1)]*Te;               % temps (part de 0, avance de Te en Te, et contient N points
+
+
+Freq = [0:N-1]/(N*Te);          %l'espace des fréquences
+
+subplot(2,1,1);
+plot(t,M1) ;
+title('Représentation temporelle');
+
+
+subplot(2,1,2);
+plot(Freq, abs(fft(M1)));       % tracé de l'amplitude en fonction de la fréquence
+title('Représentation fréquentielle');
+
+
+
